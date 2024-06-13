c3260fdc8372d1e4c0738d40e27c17060e3514c9
randyuncle <74038554+randyuncle@users.noreply.github.com>
2024-06-14 00:59:28 +0800

    Add: Introduce Deep Image Prior (DIP)
    This commit introduces the Deep Image Prior (DIP) method for standard
    image inverse tasks, as presented by Dmitry Ulyanov, Andrea Vedaldi,
    and Victor Lempitsky in 2017[1].
    
    The DIP code is an initial version inspired primarily by the source code
    from the original DIP inventors[1] and a clean, rewritten version by
    RPraneetha's GitHub repository[2].
    
    `deep_image_prior/unet.py` constructs a U-net-like hourglass
    (encoder-decoder) architecture with skip connections. The overall
    structure is based on RPraneetha's work[2], which improved the clarity
    of the skip architecture inspired by the original DIP source code[1].
    Instead of combining skip connections and up-down sampling in a single
    block of code, these elements are separated into distinct classes,
    making the architecture easier to read and debug.
    
    However, RPraneetha's implementation only supports a single
    architecture, without the ability to switch between different layer
    implementations. To address this, I introduced the convolutional layer
    construction function `conv()` from the DIP source code[1] into
    `deep_image_prior/unet.py` and replaced RPraneetha's convolutional layer
    with this function. Additionally, the class and function arguments in
    the `deep_image_prior` directory mostly follow the original authors[1],
    allowing for easy switching between different layer architectures for
    research purposes.
    
    Image import, training, and validation are implemented in the Python
    script `main.ipynb`, based on the original authors' work[1] in their
    denoising task. The main difference is the adjustment of the
    hyperparameter for the number of skip connections. The original
    declaration of 4 skip connections caused issues in my experiments, so I
    increased it to 128 skip connections, which resolved the problem.
    
    Reference:
     [1] Deep image prior
     - code: https://github.com/DmitryUlyanov/deep-image-prior
     - project page: https://dmitryulyanov.github.io/deep_image_prior
    
     [2] Reproducing Inpainting and Image Restoration from Deep Image Prior
     https://github.com/RPraneetha/DL-RP41-Deep-Image-Prior/tree/master

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Generative-Models-for-Visual-Signals
-The homework from GAI course in NCKU CSIE
+# Generative Models for Visual Signals
+
+This project aims to take a deep look into the implementation of accelerating the initialization of Denoising Diffusion Probabilistic Model (DDPM) training process by applying the initial prior strategy of Deep Image Prior (DIP).
+
+## Development Environment
+
+* OS: Windows 10 Home
+* GPU: NVIDEA GeForce RTX 3070
+
+<!-- ## Main Progress
+
+## Experiment
+
+## Conclusion -->
---
+++ deep_image_prior/unet.py
@@ -0,0 +1,151 @@
+import torch.nn as nn
+import torch as torch
+
+# The convolution layer built from the source code
+def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+    downsampler = None
+    if stride != 1 and downsample_mode != 'stride':
+        if downsample_mode == 'avg':
+            downsampler = nn.AvgPool2d(stride, stride)
+        elif downsample_mode == 'max':
+            downsampler = nn.MaxPool2d(stride, stride)
+        else:
+            assert False
+
+        stride = 1
+
+    padder = None
+    to_pad = int((kernel_size - 1) / 2)
+    if pad == 'reflection':
+        padder = nn.ReflectionPad2d(to_pad)
+        to_pad = 0
+  
+    convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
+
+    layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+    return nn.Sequential(*layers)
+
+###
+# The encoder-decoder structure similar to the original paper
+# The following architecture refers to the following github repo:
+# https://github.com/RPraneetha/DL-RP41-Deep-Image-Prior/tree/master
+###
+
+class DownSample(nn.Module):
+    def __init__(self, in_size, out_size, kernel, pad, desample_mode):
+        super(DownSample, self).__init__()
+
+        self.act = nn.LeakyReLU(0.2, inplace=True)
+        self.batch_norm = nn.BatchNorm2d(out_size)
+        self.cnn1 = conv(in_size, out_size, kernel, 1, True, pad, desample_mode)
+        self.cnn2 = conv(out_size, out_size, kernel, 1, True, pad, desample_mode)
+    
+    def forward(self, input):
+        output = self.cnn1(input)
+        output = self.batch_norm(output)
+        output = self.act(output)
+        
+        output = self.cnn2(output)
+        output = self.batch_norm(output)
+        output = self.act(output)
+        
+        return output
+
+class UpSample(nn.Module):
+    def __init__(self, in_size, out_size, kernel, pad, upsample_mode):
+        super(UpSample, self).__init__()
+        # self.conv = UnetConv2(in_size, out_size, norm_layer, need_bias, pad)
+        # self.down = nn.MaxPool2d(2, 2)
+        
+        self.act = nn.LeakyReLU(0.2, inplace=True)
+        self.batch_norm = nn.BatchNorm2d(out_size)
+        self.batch_norm_fixed = nn.BatchNorm2d(in_size)
+        self.cnn1 = conv(in_size, out_size, kernel, 2, True, pad, 'stride')
+        self.cnn2 = conv(out_size, out_size, 1, 1, True, pad, 'stride')
+        self.upsample = nn.Upsample(scale_factor=2, mode=upsample_mode)
+
+    def forward(self, input):
+        output = self.batch_norm_fixed(input)
+        
+        output = self.cnn1(output)
+        output = self.batch_norm(output)
+        output = self.act(output)
+        
+        output = self.cnn2(output)
+        output = self.batch_norm(output)
+        output = self.act(output)
+        
+        output = self.upsample(output)
+        
+        return output
+
+class SkipConnection(nn.Module):
+    def __init__(self, in_size, out_size, kernel, pad):
+        
+        super(SkipConnection, self).__init__()
+        self.act = nn.LeakyReLU(0.2, inplace=True)
+        self.batch_norm = nn.BatchNorm2d(out_size)
+        self.cnn = conv(in_size, out_size, kernel, 1, True, pad)
+
+    def forward(self, input):
+        output = self.cnn(input)
+        output = self.batch_norm(output)
+        output = self.act(output)
+        
+        return output
+        
+class SkipArchitecture(nn.Module):
+    def __init__(self, num_input_channels=2, num_output_channels=3, 
+                num_channels_down=[16, 32, 64, 128, 128], num_channels_up=[16, 32, 64, 128, 128], num_channels_skip=[4, 4, 4, 4, 4], 
+                filter_size_down=3, filter_size_up=3, filter_skip_size=1,
+                need_sigmoid=True, need_bias=True, 
+                pad='zero', upsample_mode='nearest', downsample_mode='stride'):
+        super(SkipArchitecture, self).__init__()
+        
+        assert len(num_channels_down) == len(num_channels_up) == len(num_channels_skip)
+        self.n_scales = len(num_channels_down)
+        
+        self.down_sampler = nn.ModuleList([DownSample(in_size=num_input_channels if i == 0 
+                                                              else num_channels_down[i-1], 
+                                                      out_size=num_channels_down[i], 
+                                                      kernel=filter_size_down,
+                                                      pad=pad, 
+                                                      desample_mode=downsample_mode) 
+                                           for i in range(len(num_channels_down))])
+        self.up_sampler = nn.ModuleList([UpSample(in_size=num_channels_skip[i] + num_channels_up[i + 1] if i != len(num_channels_down) - 1
+                                                                    else num_channels_up[i],
+                                                  out_size=num_channels_up[i],
+                                                  kernel=filter_size_up,
+                                                  pad=pad,
+                                                  upsample_mode=upsample_mode)
+                                          for i in range(len(num_channels_up)-1, -1, -1)])
+        self.skip_connections = nn.ModuleList([SkipConnection(in_size=num_channels_down[i], 
+                                                              out_size=num_channels_skip[i], 
+                                                              kernel=filter_skip_size,
+                                                              pad=pad)
+                                               for i in range(len(num_channels_up))])
+        
+        self.last_cnn = conv(num_channels_up[0], num_output_channels, 1, bias=need_bias, pad=pad)
+        self.sigmoid = nn.Sigmoid() if need_sigmoid else None
+
+    def forward(self, input):
+        skip = []
+        
+        for i in range(self.n_scales):
+            # print(self.down_sampler[i])
+            input = self.down_sampler[i].forward(input)
+            skip.append(self.skip_connections[i].forward(input))
+            
+        for i in range(self.n_scales):
+            if i == 0:
+                # print(input)
+                # print(self.up_sampler[i])
+                input = self.up_sampler[i].forward(skip[-1])
+            else:
+                input = self.up_sampler[i].forward(torch.cat([input,skip[self.n_scales - i - 1]], 1))
+
+        output = self.last_cnn(input)
+        if self.sigmoid != None:
+            output = self.sigmoid(output)
+        
+        return output
